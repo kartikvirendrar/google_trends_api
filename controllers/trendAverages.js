ee468b6a41687b3236c6d14dfa6eb47894652ea0
Kartik Virendra Rajput <88619994+kartikvirendrar@users.noreply.github.com>
2022-10-16 22:35:11 +0530

    Update trendAverages.js

--- controllers/trendAverages.js
+++ controllers/trendAverages.js
@@ -7,12 +7,15 @@ exports.trendAverages = async (req, res) => {
         let outputArray = [];
         let time2 = new Date();
         let time1 = new Date(time2.getFullYear()-1, time2.getMonth(), time2.getDate());
-        for (let i = 0; i < Math.ceil(keywords.length/5); i++) {
-            let subKey = keywords.slice((i*5), (i*5)+5);
+        for (let i = 0; i < Math.ceil(keywords.length/4); i++) {
+            let subKey = keywords.slice((i*4), (i*4)+4);
+            subKey.push("google");
             await googleTrends.interestOverTime({keyword: subKey, startTime:time1, endTime:time2, geo:"US"})
             .then((data) => {
                 let jsonData = JSON.parse(data);
                 let temp = jsonData.default.averages;
+                console.log(temp);
+                temp.pop();
                 temp.forEach(element => {
                     outputArray.push(element);                      
                 });
